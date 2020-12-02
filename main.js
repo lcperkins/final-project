5de1247a9581c81cd5d2b2e549f5453cfffe49a0
Lance Perkins <lcperkins@mail.fhsu.edu>
2020-12-02 16:23:28 -0600

    Took out most of code and added comment

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const api = "30f300f272a3eb8f72d03e9238797d23";
 const base = "https://api.openweathermap.org/data/2.5/";
 
+// For the life of me, I could not make this thing work, so I wiped most everything I had because it was a mess.
+
 
 function getWeather (query) {
     fetch(`${base}weather?q=${query}&units=imperial&APPID=${api}`)
